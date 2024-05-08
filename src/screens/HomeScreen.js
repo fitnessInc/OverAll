262ea4bde3d90bdc6ab34fb84a328cafee642240
Adnane <nanitosse@gmail.com>
2024-05-08 13:28:24 -0400

    PASSPORTLOGO

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,41 +8,7 @@ import nutrition from '../../assets/images/nutrution.jpg';
 
 const HomeScreen = () => {
 
-//   return (
-//     <SafeAreaView>
-//       <ScrollView>
-//         <Card containerStyle={{
-//           // shadowColor: 'rgba(0,0,0, .2)',
-//           // shadowOpacity: 0, //default is 1
-//           // shadowRadius: 0,
-//           // borderWidth: 0,
-//           // borderColor: 'transparent',
-//           // height: 300,
-//           // shadowOffset: { height: 0, width: 0 },
 
-
-//         }}>
-//           <Card.Title >CAlisthenics</Card.Title>
-//           <Card.Divider style={{marginTop:0,height:0}} />
-//           <View style={{}}>
-//             <Image
-//               style={{ width: 270, height: 200, borderLeftWidth:100, }}
-//               esizeMode="cover"
-//               source={require('../../assets/images/calisImg.jpg')}
-//             />
-//           </View>
-//         </Card>
-//       </ScrollView>
-//     </SafeAreaView>
-    
-
-
-
-
-
-
-
-//   )
 
  return (
   <ScrollView style={{ paddingVertical: 10 }}>
@@ -76,7 +42,7 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
 space: {
-  marginVertical: 20, // Adjust the margin as needed
+  marginVertical: 20, 
 },
 });
 
--- src/screens/LoggingScreen.js
+++ src/screens/LoggingScreen.js
@@ -1,260 +1,116 @@
+import React from "react";
+import { Input, Button, Icon, Text } from "react-native-elements";
+import { View, ImageBackground, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { Svg } from "react-native-svg";
+import { Dimensions } from 'react-native';
+import { BackgroundImage } from "@rneui/base";
+import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
-
-import { Text, View, Modal, StyleSheet, Alert, Image } from "react-native"
-import { useState } from "react";
-import { Input, Button, Icon } from "react-native-elements";
-import * as MailComposer from 'expo-mail-composer';
-import * as ImagePicker from 'expo-image-picker';
-import glogo from '../../assets/images/glogo.jpg';
-import *as ImageManipulator from 'expo-image-manipulator';
 
 
 const LoggingScreen = () => {
-    const [modal, setModal] = useState(false);
-    const [name, setNAme] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
-    const [imageUrl, setImageUrl] = useState( 'images/glogo.jpg');
 
-    const processImage = async (imgUri) => {
-        const image = await ImageManipulator.manipulateAsync(
-            imgUri,
-            [{ resize: { width: 400 } }],
-            { format: ImageManipulator.SaveFormat.PNG }
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
+    const Submit = () => {
+        console.log("Username:", username);
+        console.log("Password:", password);
 
-        );
-
-        setImageUrl(image.uri);
-
-        console.log(processImage)
-
-
-
-    }
-    const getImageFromGallery = async () => {
-        const mediaLibraryPermissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-        if(mediaLibraryPermissions.status==='granted'){
-            const  capturedImage = await ImagePicker.launchImageLibraryAsync({
-                allowsEditing: true,
-                aspect: [1, 1]
-            });
-
-            if(!capturedImage.canceled){
-                console.log(capturedImage);
-    
-                processImage(capturedImage.uri);
-            }
-
-
-        }
-
-       
-    }
-
-    const sendMail = () => {
-        MailComposer.composeAsync({
-            recipients: ['nanitosse@gmail.com'],
-            subject: 'Inquiry',
-            body: 'To whom it may concern:'
-
-        });
-    }
-
-    const print = () => {
-        Alert.alert('pleas enter your contact')
-
-    }
-
-    const resetForm = () => {
-        setNAme(''),
-            setPhone('')
-        setEmail('')
-        setAddress('')
-    }
-    const handelSubmit = () => {
-        console.log({
-            Name: name,
-            Phone: phone,
-            Email: email,
-            Address: address
-
-
-        })
-
-
-
-    }
-    const getImageFromCamera = async () => {
-        const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
-        if (cameraPermission.status === 'granted') {
-            const capturedImage = await ImagePicker.launchCameraAsync({
-                allowsEditing: true,
-                aspect: [1, 1]
-
-            });
-            if (!capturedImage.canceled) {
-                console.log(capturedImage);
-              
-                processImage(capturedImage.uri);
-            }
-
-        }
     }
 
     return (
-        <View style={styles.centeredView}>
 
-            <View style={{ marginBottom: 4, }}>
-                <Text>Press the Button to Open the Registration  Form</Text>
-                <Button
-                    onPress={() => setModal(true)}
-                    // style={styles.hide}
-                    title='OPEN'
-                    style={styles.hide}
+
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Username"
+                    onChangeText={text => setUsername(text)}
+                    value={username}
                 />
+                <Ionicons name="person" size={24} color="black" style={styles.icon} />
             </View>
-            <Modal
-
-                animationType="slide"
-                visible={modal}
-                transparent={true}
-                onShow={() => print()}
-            >
-                <View style={{ backgroundColor: 'transparent', flex: 1 }}>
-                    <View style={{ backgroundColor: 'white', borderRadius: 10, flex: 1, marginTop: -105 }}>
-                        <View style={{ marginTop: 90 }}>
-                            <Image
-                                source={{ uri: imageUrl }}
-                                loadingIndicatorSource={glogo}
-                                style={{ marginTop: 90, width: 70, height: 70 }}
-                            />
-                            <Button
-                                title="Camera" onPress={getImageFromCamera}
-                            />
-                            <Button
-                                title='Galery'
-                                onPress={getImageFromGallery}
-                            />
-
-                        </View>
-                        <Input
-                            placeholder="Full-Name"
-                            leftIcon={{ type: 'font-awesome', name: 'user-o' }}
-                            onChangeText={(name) => setNAme(name)}
-                            value={name}
-                        />
-                        <Input
-                            placeholder="Phone Number"
-                            // leftIcon={{ type: 'font-awesome', name: 'mobile-o' }}
-                            onChangeText={(phone) => setPhone(phone)}
-                            value={phone}
-                        />
-                        <Input
-                            placeholder="Email"
-                            leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
-                            onChangeText={(email) => setEmail(email)}
-                            value={email}
-                        />
-                        <Input
-                            placeholder="Address"
-                            leftIcon={{ type: 'font-awesome', name: 'address-card' }}
-                            onChangeText={(address) => setAddress(address)}
-                            value={address}
-                        />
-                        <View>
-                            <Button
-                                onPress={() => setModal(false)}
-                                style={{ color: 'blue', justifyContent: 'center', marginTop:-10, padding: 15 }}
-                                title='Cancel'
-                            />
-                        </View>
-                        <View>
-                            <Button
-                                onPress={() => {
-                                    handelSubmit();
-                                    resetForm();
-                                }}
-                                style={{ color: 'blue', justifyContent: 'center', marginTop: -5, padding: 15 }}
-                                title='Submit'
-                            />
-                            <Button
-                                onPress={() => sendMail()}
-                                title='Send Email'
-                                style={{ marginTop: -20, backgroundColor: 'transparent', justifyContent: 'center', padding: 15 }}
-                                icon={
-                                    <Icon
-                                        name='envelope-o'
-                                        type='font-awesome'
-                                        color='black'
-                                        iconStyle={{ marginright: 10 }}
-
-                                    />
-                                }
-                            />
-                        </View>
-
-                    </View>
-                </View>
-            </Modal>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    onChangeText={text => setPassword(text)}
+                    value={password}
+                    secureTextEntry={true}
+                />
+                <Ionicons name="lock-closed" size={24} color="black" style={styles.icon} />
+            </View>
+            <TouchableOpacity style={styles.button} onPress={Submit}>
+                <Text style={styles.buttonText}>Log In</Text>
+                <Text style={{ margin: 10, justifyContent: 'center' }}>Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <View style={styles.socialIcons}>
+                <Ionicons name="logo-facebook" size={24} color="blue" style={styles.socialIcon} />
+                <Ionicons name="logo-google" size={24} color="red" style={styles.socialIcon} />
+                <Ionicons name="logo-linkedin" size={24} color="purple" style={styles.socialIcon} />
+            </View>  
+            </TouchableOpacity>
         </View>
 
 
 
 
-
-
-
-
     )
+
 }
 
+
+
 const styles = StyleSheet.create({
-    centeredView: {
+
+
+    container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 22,
     },
-    container: {
-        modal: {
-            justifyContent: 'center',
-            margin: 0,
 
-        },
-        text: {
-            color: 'yellow',
-            justifyContent: 'center',
-            fontWeight: 'bold',
-            fontStyle: 'italic'
-        },
-        button: {
-            color: 'blue',
-            justifyContent: 'center',
-            padding: 0,
-        },
-        hide: {
-            color: 'transparent',
-            padding: 0,
-            justifyContent: 'center',
-            marginTop: 0,
-        },
-        imageContainer: {
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-evenly',
-            marginTop: 0
-        },
-        // image:{
-        //     widht:30,
-        //     height:30
+    input: {
+        height: 50,
+        width: '200',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 100,
+        marginBottom: 0,
+    },
+    button: {
+        backgroundColor: 'transparent',
+        padding: 10,
+        borderRadius: 3,
+        alignContent: 'center',
+        textAlign: 'right'
+    },
+    buttonText: {
+        color: 'black',
+        fontSize: 20,
+        textAlign: 'center'
 
-        // }
+
+    },
+    icon: {
+        position: 'absolute',
+        right: 10,
+    },
+    socialIcons: {
+        flexDirection: 'row',
+        marginTop: 10,
+        
+    },
+    socialIcon: {
+        marginHorizontal: 10,
     }
-})
 
+
+})
 
 export default LoggingScreen;
