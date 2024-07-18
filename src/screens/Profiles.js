49851ea0c6bb1830a56bee0dd7f10d48fd12d859
Adnane <nanitosse@gmail.com>
2024-07-17 22:09:32 -0400

    profilePart1

--- src/screens/Profiles.js
+++ src/screens/Profiles.js
@@ -1,14 +1,17 @@
+import { width } from "deprecated-react-native-prop-types/DeprecatedImagePropType";
 import React, { useState } from "react";
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image, SafeAreaView } from "react-native";
 import { ListItem, Avatar, Button } from 'react-native-elements'
-import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+// import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
+
+import * as ImagePicker from 'expo-image-picker';
 
 
 
 
 const data = [
     {
-        id: '1',
+        id: '3',
         Name: "nanito",
         subTitle: 'trainer',
         uri: "null"
@@ -16,9 +19,16 @@ const data = [
 
     },
     {
-        id: '2',
+        id: '4',
         Name: "Mouna",
         subTitle: 'trainer',
+        uri: "null"
+
+    },
+    {
+        id: '5',
+        Name: "tonton",
+        subTitle: 'trainee',
         uri: "null"
 
     },
@@ -32,88 +42,130 @@ const data = [
 
 const Profiles = () => {
     const [avatars, setAvatars] = useState(data);
+    const ItemSeparatorComponent = () => <View style={{}} />
 
-        const chooseImage = (id) => {
-            const options = {
-                mediaType: 'photo',
-                quality: 1,
-                saveToPhotos: true
-            };
+    const Pickimage = async (id) => {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+            return;
 
-            launchImageLibrary(options, (response) => {
-                if (response.didCancel) {
-                    console.log('User cancelled image picker');
-                } else if (response.errorCode) {
-                    Alert.alert('Error', response.errorMessage);
-                } else {
-                    const source = { uri: response.assets[0].uri };
-                    setAvatars((prev) => ({ ...prev, [id]: source }));
-                }
-            });
-        };
+        }
 
-
-      const takePhoto = (id) => {
-        const options = {
-            mediaType: 'photo',
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
             quality: 1,
-        };
-        launchCamera(options, (response) => {
-            if (response.didCancel) {
-                console.log('User cancelled camera');
-            } else if (response.errorCode) {
-                Alert.alert('Error', response.errorMessage);
-            } else {
-                const source = { uri: response.assets[0].uri };
-                setAvatars((prev) => ({ ...prev, [id]: source }));
-            }
         });
 
-      } 
+        if (!result.canceled) {
+            const newData = avatars.map(item => {
+                if (item.id === id) {
+                    // return { ...item, uri: result.assets[0].uri };
+                    return { ...item, uri: result.uri };
+                }
+                  console.log(result)
+                return(
+                    item
+                    
+                ) 
+                
+            });
+            setAvatars(newData);
+        }
+    };
+
+
+       
+    
+
+
+   
+
+    
 
 
 
 
-        const renderItem = ({ item }) => (
-            <ListItem bottomDivider>
-                <TouchableOpacity onPress={() => chooseImage(item.id)}>
-                    {avatars[item.id] ? (
-                        <Image source={avatars[item.id]} style={styles.avatar} />
-                    ) : (
-                        <Avatar
-                            size="large"
-                            icon={{ name: 'person', type: 'material', color: 'white' }}
-                            overlayContainerStyle={{ backgroundColor: 'grey' }}
+    const renderItem = ({ item }) => (
 
-                        />
-                    )}
-                </TouchableOpacity>
-                <ListItem.Content>
-                    <ListItem.Title>{item.Name}</ListItem.Title>
-                    <ListItem.Title>{item.subTitle}</ListItem.Title>
-                </ListItem.Content>
-                <Button title="Photo" onPress={() => takePhoto(item.id)} />
-            </ListItem>
-        );
 
-        return (
+
+        <ListItem
+            bottomDivider
+            style={{color:'green'}}
+
+        >
+            <TouchableOpacity onPress={() => Pickimage(item.id)}>
+                {avatars[item.id] ? (
+                    <Image source={avatars[item.id]} style={styles.avatar} />
+                ) : (
+                    <Avatar
+                        size="medium"
+                        icon={{ name: 'person', type: 'material', color: 'white' }}
+                        overlayContainerStyle={{ backgroundColor: 'black' }}
+                        rounded
+
+                    />
+                )}
+            </TouchableOpacity>
+            <ListItem.Content>
+                <ListItem.Title>{item.Name}</ListItem.Title>
+                <ListItem.Title>{item.subTitle}</ListItem.Title>
+            </ListItem.Content>
+          
+        </ListItem>
+
+
+    )
+
+
+
+
+    return (
+        <SafeAreaView
+            style={{ flex: 1, marginTop:70}}
+        >
             <FlatList
                 data={data}
                 keyExtractor={(item) => item.id}
                 renderItem={renderItem}
+                ItemSeparatorComponent={ItemSeparatorComponent}
+
+
+
             />
+        </SafeAreaView >
+
     )
 
 
-   
+
+
 }
 
-const styles = StyleSheet.create({
-    avatar: {
-        width: 40,
-        height: 40,
 
+const styles = StyleSheet.create({
+
+    avatar: {
+        width: 50,
+        height: 50,
     },
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
 
 export default Profiles
