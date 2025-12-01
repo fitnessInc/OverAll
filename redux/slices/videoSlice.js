199797f85c3fcf4e9dd5b6892501d6b3df8f0bd6
Adnane <nanitosse@gmail.com>
2025-12-01 15:39:05 -0500

    redux-Slice-for-Multiple-fils

--- redux/slices/videoSlice.js
+++ redux/slices/videoSlice.js
@@ -1,16 +1,38 @@
 
 import { createSlice } from '@reduxjs/toolkit';
-import {Asset } from 'expo-asset';
+import { Asset } from 'expo-asset';
 const videoAsset = Asset.fromModule(require('../../assets/videos/nutrution.mp4'))
+
+// const initialState = {
+//   metaPro: {
+//     'user1': '../../assets/videos/nutrution.mp4',
+//     'user2': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+//     'user3': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+//     'user4': 'https://randomuser.me/api/portraits/men/1.jpg',
+//   }
+// };
 
 const initialState = {
   metaPro: {
-    'user1': '../../assets/videos/nutrution.mp4',
-    'user2': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    'user3': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    'user4': 'https://randomuser.me/api/portraits/men/1.jpg',
+    user1: [
+      '../../assets/videos/nutrution.mp4',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      'https://randomuser.me/api/portraits/men/1.jpg',
+    ],
+    user2: [
+      '../../assets/videos/nutrution.mp4',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      'https://randomuser.me/api/portraits/men/1.jpg',
+
+    ],
+    user3: [
+      '../../assets/videos/nutrution.mp4',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      'https://randomuser.me/api/portraits/men/1.jpg',
+
+    ]
   }
-};
+}
 
 const metaSlice = createSlice({
   name: 'meta',
@@ -18,7 +40,12 @@ const metaSlice = createSlice({
   reducers: {
     metaProfile: (state, action) => {
       const { id, newMeta } = action.payload;
-      state.metaPro[id] = newMeta;
+
+      if (!state.metaPro[id]){
+         state.metaPro[id]=[]
+      }
+    
+      state.metaPro[id].push(newMeta)
     }
   }
 });
