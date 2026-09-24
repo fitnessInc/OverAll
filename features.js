6fc7a26ac7a8b31c0c82be9b52f242ffe8b22cc4
fitnessInc <mouladasher@gmail.com>
2026-09-24 11:18:52 -0400

    Update features.js

--- features.js
+++ features.js
@@ -1,1 +1,86 @@
+<View style={styles.container}>
+          <View style={styles.box}>
+            <TouchableOpacity activeOpacity={0.7} onPress={openModal}>
+              <LinearGradient
+                colors={['white', 'silver', 'white']}
+                start={{ x: 3, y: -2 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.button}
+              >
+                <Text style={styles.text}>Book Now</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Modal
+                animationType="fade"
+                transparent={false}
+                visible={modal}
+                onRequestClose={closeModal}
 
+              >
+
+                <View style={styles.modalOverlay}>
+                  <Text style={styles.text}>Choose Number of trainees</Text>
+                  <Picker
+                    style={styles.Picker}
+                    selectedValue={trainees}
+                    onValueChange={trainer}
+                    itemStyle={styles.itemStyle}
+
+                  >
+                    <Picker.Item label='1' value={1} />
+                    <Picker.Item label='2' value={2} />
+                    <Picker.Item label='3' value={3} />
+                    <Picker.Item label='4' value={4} />
+                    <Picker.Item label='5' value={5} />
+                    <Picker.Item label='6' value={6} />
+                  </Picker>
+                  <Separator />
+                  <Text style={styles.text}>Choose location</Text>
+                  <Picker
+                    style={styles.Picker}
+                    selectedValue={location}
+                    onValueChange={Spot}
+                    itemStyle={styles.itemStyle}
+                  >
+                    <Picker.Item label='24Fit' value='24Fit' />
+                    <Picker.Item label='jerseyFit' value='jerseyFit' />
+                    <Picker.Item label='bayonFit' value='bayonFit' />
+                    <Picker.Item label='FourFit' value='FourFit' />
+                  </Picker>
+                  <Separator />
+                  <Picker
+                    style={styles.Picker}
+                    selectedValue={selectedValue}
+                    onValueChange={(itemValue, itemIndex) => {
+                      setSelectedValue(itemValue);
+                      itemValue === 'datetime' ? setShowpicker(true) : undefined
+
+                    }}
+
+                  >
+                    <Picker.Item label="Choose Date and Time" value="datetime" />
+                    <Picker.Item label="Select an option" value="default" />
+                  </Picker>
+                  {showPicker && (
+                    <DateTimePicker
+                      value={date}
+                      mode="datetime"
+                      is24Hour={true}
+                      display="default"
+                      onChange={onChange}
+                    />
+                  )}
+                  <Button
+                    title="CLOSE"
+                    onPress={closeModal}
+                    buttonStyle={{ backgroundColor: 'rgba(0,10,0,0.2)', borderRadius: 10, padding: 10 }}
+                    containerStyle={{ marginTop: 200 }}
+                    titleStyle={{ color: 'black', fontWeight: 'bold' }}
+                  />
+
+                </View>
+              </Modal>
+            </TouchableOpacity>
+          </View>
+        </View>
